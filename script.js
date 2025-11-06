240a95705a74e0bd6b5b6a6284b689287eb769cb
Abhishek Kumar Sinha <165494965+AbhishekSinhaa17@users.noreply.github.com>
2025-11-06 18:10:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,5 +12,17 @@ const getSum = () => {
     total += value;
 )};
 
+  const table = document.querySelector("table");
+  const totalRow = document.createElement("tr");
+  const totalCell = document.createElement("td");
+
+  totalCell.setAttribute("colspan", "2");
+  totalCell.style.fontWeight = "bold";
+  totalCell.textContent = `Total Price: Rs ${total}`;
+
+  totalRow.appendChild(totalCell);
+  table.appendChild(totalRow);
+};
+
 getSumBtn.addEventListener("click", getSum);
 
