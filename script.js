9690e12ef0ce65fd3b83729dc5ce774864ec9ac5
Abhishek Kumar Sinha <165494965+AbhishekSinhaa17@users.noreply.github.com>
2025-11-06 18:10:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
-};
+   const priceCells = document.querySelectorAll(".prices");
+  let total = 0;
+
+  priceCells.forEach(cell => {
+    const value = parseFloat(cell.textContent) || 0;
+    total += value;
+)};
 
 getSumBtn.addEventListener("click", getSum);
 
